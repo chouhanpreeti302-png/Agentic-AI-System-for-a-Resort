1720ef87f67ddbaccb172ad152266730179e9a6d
Preeti Chouhan <chouhanpreeti302@gmail.com>
2025-12-26 17:21:01 +0530

    add RECEPTION_INFO for check-in/check-out times and facilities

---
+++ app/data/resort_info.py
@@ -0,0 +1,9 @@
+RECEPTION_INFO = {
+    "check_in_time": "3:00 PM",
+    "check_out_time": "11:00 AM",
+    "facilities": {
+        "gym": "Open 6:00 AM - 10:00 PM. Located on Level 2.",
+        "spa": "Open 10:00 AM - 8:00 PM. Prior booking recommended.",
+        "swimming_pool": "Open 7:00 AM - 9:00 PM. Towels available poolside.",
+    },
+}
